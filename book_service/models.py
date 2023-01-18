316acc42abe64297c6498e36aad229e21cad6535
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-18 21:36:27 +0300

    CRUD and endpoints for books was implemented

--- book_service/models.py
+++ book_service/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=4, choices=CoverChoices.choices)
+    cover = models.CharField(max_length=4, choices=CoverChoices.choices, default="Hard")
     inventory = models.IntegerField()
     daily_fee = MoneyField(max_digits=4, decimal_places=2, default_currency="USD")
 
