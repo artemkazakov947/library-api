7aaf3b948cef5d36dde10da5c63bc06bbd26e019
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-18 19:27:05 +0300

    Added book model and migrations.

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.TextChoices("cover", "HARD SOFT")
+    inventory = models.IntegerField()
+    daily_fee = MoneyField(max_digits=4, decimal_places=2, default_currency="USD")
+
+    def __str__(self) -> str:
+        return f"Book {Book.title} by {Book.author}"
