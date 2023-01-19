cbd6770bf2d709e6d4272cf8aaa2b5594ee35f63
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-19 20:08:20 +0300

    Customs permissions were added

--- book_service/views.py
+++ book_service/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 
 from book_service.models import Book
+from book_service.permissions import IsAdminOrIsAuthenticatedOrAnonReadOnly, IsAdminOrIsAuthenticatedReadOnly
 from book_service.serializers import BookSerializer
 
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIsAuthenticatedOrAnonReadOnly, )
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIsAuthenticatedReadOnly, )
