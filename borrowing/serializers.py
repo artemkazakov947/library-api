1c830aeb0f80bff1ccd3332a77b7b1d9eab16238
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-20 12:32:22 +0300

    Added list and detail serializers for Borrowing

---
+++ borrowing/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from book_service.serializers import BookSerializer
+from borrowing.models import Borrowing
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book_title = serializers.CharField(source="book_id.title", read_only=True)
+    book_author = serializers.CharField(source="book_id.author", read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return", "actual_return", "book_title", "book_author", "user_id")
+
+
+class BorrowingDetailSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many=False, read_only=True)
