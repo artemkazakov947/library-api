b3423e52837edcbe1e9dc58f30305c0d665dc818
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-24 12:52:52 +0300

    Method get_queryset was added. Non-admins user can see only their borrowings.

--- borrowing/views.py
+++ borrowing/views.py
@@ -9,13 +9,18 @@ from borrowing.serializers import (
 
 
 class BorrowingListView(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
 
     def get_serializer_class(self):
         if self.request.method == "GET":
             return BorrowingListSerializer
         return BorrowingCreateSerializer
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.all()
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user_id=self.request.user)
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
