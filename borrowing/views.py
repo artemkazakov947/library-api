e8c33333bc1a1e71a9515e0d509f5bdaf423bcb7
Artem Kazakov <artemkazakov947@gmail.com>
2023-01-24 10:59:10 +0300

    BorrowingListView was modified by add ListCreateAPIView and get_serializer_class

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,12 +1,21 @@
 from rest_framework import generics
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingListSerializer, BorrowingDetailSerializer
+from borrowing.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
+)
 
 
-class BorrowingListView(generics.ListAPIView):
+class BorrowingListView(generics.ListCreateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return BorrowingListSerializer
+        return BorrowingCreateSerializer
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
