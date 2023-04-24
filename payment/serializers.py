cd2d999b25e6894f1f8f672b464b7ee0de0439b6
Artem Kazakov <artemkazakov947@gmail.com>
2023-04-24 19:23:15 +0200

    Created PaymentBaseSerializer

--- payment/serializers.py
+++ payment/serializers.py
@@ -4,7 +4,22 @@ from borrowing.serializers import BorrowingDetailSerializer
 from payment.models import Payment
 
 
-class PaymentListSerializer(serializers.ModelSerializer):
+class PaymentBaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
+
+    def get_money_to_pay(self, obj):
+        return obj.money_to_pay
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret["money_to_pay"] = str(ret["money_to_pay"])
+        return ret
+
+
+class PaymentListSerializer(PaymentBaseSerializer):
     user_id = serializers.SlugRelatedField(source="borrowing.user", slug_field="id", many=False, read_only=True)
     book = serializers.SlugRelatedField(source="borrowing.book", slug_field="title", many=False, read_only=True)
 
@@ -13,26 +28,9 @@ class PaymentListSerializer(serializers.ModelSerializer):
 
         fields = ("id", "user_id", "book", "borrowing", "status", "type", "money_to_pay")
 
-    def get_money_to_pay(self, obj):
-        return obj.money_to_pay
 
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        ret["money_to_pay"] = str(ret["money_to_pay"])
-        return ret
-
-
-class PaymentDetailSerializer(serializers.ModelSerializer):
+class PaymentDetailSerializer(PaymentBaseSerializer):
     borrowing = BorrowingDetailSerializer(many=False, read_only=True)
-
-    def get_money_to_pay(self, obj):
-        return obj.money_to_pay
-
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        ret["money_to_pay"] = str(ret["money_to_pay"])
-        return ret
-
 
     class Meta:
         model = Payment
